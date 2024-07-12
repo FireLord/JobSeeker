6e5f51ef6e94de830ee4c4d90d2421c7526993ee
FireLord <firelord.xda@gmail.com>
2024-07-12 23:46:00 +0530

    homeScreen: Add paging error and load state

--- app/src/main/java/com/firelord/jobseeker/presentation/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/firelord/jobseeker/presentation/ui/screen/home/HomeScreen.kt
@@ -10,17 +10,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
@@ -100,6 +104,34 @@ class HomeScreen: Screen {
                                         navigator.push(DetailScreen(jobModel = jobData))
                                         navViewModel.isNavVisible.value = false
                                     }
+                                )
+                            }
+                        }
+                    }
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.End)
+                ) {
+                    jobModelListState.apply {
+                        when {
+                            loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading -> {
+                                CircularProgressIndicator(
+                                    modifier = Modifier
+                                        .size(100.dp)
+                                        .padding(16.dp)
+                                )
+                            }
+                            loadState.refresh is LoadState.Error || loadState.append is LoadState.Error -> {
+                                Text(
+                                    text = "Error: ${jobModelListState.loadState.refresh}",
+                                    style = MaterialTheme.typography.titleSmall,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Red
                                 )
                             }
                         }
