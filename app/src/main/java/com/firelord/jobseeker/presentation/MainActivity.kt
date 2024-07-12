12dda5aa2ceb05812d7367b4a795ab470fc7aadb
FireLord <firelord.xda@gmail.com>
2024-07-12 21:26:44 +0530

    presentation: Make status bar transparent

--- app/src/main/java/com/firelord/jobseeker/presentation/MainActivity.kt
+++ app/src/main/java/com/firelord/jobseeker/presentation/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(scrim = Color.WHITE, darkScrim = Color.BLACK),
+            statusBarStyle = SystemBarStyle.light(scrim = Color.TRANSPARENT, darkScrim = Color.BLACK),
             navigationBarStyle = SystemBarStyle.light(scrim = Color.WHITE, darkScrim = Color.BLACK)
         )
         setContent {
